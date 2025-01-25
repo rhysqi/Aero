250d75109ce54519f2945c0bbf271af306f7dd5e
rhysqi <rhysqi.akbar@gmail.com>
2025-01-25 08:33:20 +0700

    Rename: Aero-IO.cxx to Aero-File.cxx - Adding error handling
    submit: rhysqi

--- lib/system/Aero-File.cxx
+++ lib/system/Aero-File.cxx
@@ -5,28 +5,39 @@
 #include <Windows.h>
 #include <winuser.h>
 #include <winnt.h>
+#include <minwinbase.h>
 
-#include <fileapi.h>
+#include <memoryapi.h>
 #include <handleapi.h>
-
-#include <wchar.h>
+#include <fileapi.h>
 
 using namespace Aero_System;
 
 HANDLE File::Create(LPCWSTR FileName, LPSECURITY_ATTRIBUTES AttrSec, DWORD dwFlagsAndAttribute)
 {
+	LPSECURITY_ATTRIBUTES saFile = (LPSECURITY_ATTRIBUTES)VirtualAlloc(
+		NULL, sizeof(SECURITY_ATTRIBUTES), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE
+	);
+
+	if (saFile == NULL) {
+		Error::ShowLastError();
+		return NULL;
+	}
+
+	saFile->bInheritHandle = FALSE;
+	saFile->nLength = sizeof(SECURITY_ATTRIBUTES);
+
 	HANDLE hFile = CreateFileW(
 		FileName,
 		GENERIC_WRITE | GENERIC_READ,
-		0, AttrSec,
+		0, saFile,
 		CREATE_NEW,
 		dwFlagsAndAttribute,
 		NULL
 	);
 
 	if (hFile == INVALID_HANDLE_VALUE) {
-		DWORD errorCode = GetLastError();
-		wprintf(L"Error: Failed to make file, error code (%lu)\n", errorCode);
+		Error::ShowLastError();
 		return (HANDLE)-1;
 	}
 	
@@ -38,17 +49,17 @@ BOOL File::Write(HANDLE hFile, LPCWSTR FileBuffer)
 	BOOL hWrite = WriteFile(
 		hFile,
 		FileBuffer,
-		sizeof(FileBuffer),
+		lstrlenW(FileBuffer) * sizeof(WCHAR),
 		0,
 		NULL
 	);
-
+	
 	if (hWrite == FALSE) {
-		MessageBoxW(NULL, L"Failed to write to file!", L"Error", MB_OK);
+		Error::ShowLastError();
 		return FALSE;
 	}
 
-	return TRUE;
+	return hWrite;
 }
 
 BOOL File::Read(LPCWSTR FileName)
@@ -57,6 +68,7 @@ BOOL File::Read(LPCWSTR FileName)
 		NULL, NULL,
 		NULL, NULL, NULL
 	);
+
 	return TRUE;
 }
 
