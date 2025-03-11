f90cb9d337be22379b64d20887a3e42fc5764dda
rhysqi <rhysqi.akbar@gmail.com>
2025-03-11 16:39:08 +0700

    Update: Lib build system - Added a new build system for the library
    submit: rhysqi

--- tools/lib.mk
+++ tools/lib.mk
@@ -1,6 +1,7 @@
-lib_Filter			=	$(wildcard *.cxx)
+LIB_Filter			=	$(wildcard $(1)/*.cxx)
 
-LIB_Audio_Win32		=
-LIB_Codec_Win32		=
-LIB_Graphics_Win32	=
-LIB_System_Win32	=
+LIB_Audio_Win32		:=	$(call LIB_Filter, lib/audio)
+LIB_Codec_Win32		:=	$(call LIB_Filter, lib/codec)
+LIB_Graphics_Win32	:=	$(call LIB_Filter, lib/graphics)
+LIB_System_Win32	:=	$(call LIB_Filter, lib/system)
+LIB_Window_Win32	:=	$(call LIB_Filter, lib/window)
