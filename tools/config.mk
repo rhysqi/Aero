7ded0b8d021fd7db2d2ca956bfa466900b071085
rhysqi <rhysqi.akbar@gmail.com>
2025-03-13 13:54:23 +0700

    Update: build system config - Add linker config - Add static lib config - Add stripper config
    submit: rhysqi

--- tools/config.mk
+++ tools/config.mk
@@ -1,6 +1,8 @@
+# Compiler configuration
 CC		:=	clang
 CXX		:=	clang++
 
+# Compiler options for features flags
 OPT_F	=	-fcrash-diagnostics  -fdirectives-only -fenable-matrix \
 			-fstrict-float-cast-overflow -fstrict-vtable-pointers \
 			-fstrict-enums -fsplit-lto-unit \
@@ -11,25 +13,45 @@ OPT_Fno	=	-fno-autolink -fno-builtin -fno-cxx-modules \
 			-fno-experimental-library -fno-exceptions \
 			-fno-strict-aliasing
 
-OPT_M	=	-mavx -mavx2 -msse -msse4.2 \
+# Compiler options for machine flags
+OPT_M	=	-mavx2 -msse -msse4.2 \
 			-mlvi-cfi -mseses \
 			-mstack-arg-probe -mstackrealign
 
-OPT_Mno	=	-mno-sse2 -mno-sse3
-			
+OPT_Mno	=	-mno-fmv
+
+# Compiler options for warning flags			
 OPT_W	=	-Wall -Wabi -Wclass-conversion -Wdeprecated -Wformat-security \
 			-Wunused -Wuninitialized -Wextra
 
 OPT_Wno	=	-Wno-pedantic
 
+# Compiler options for Windows linker flags
 OPT_Wl	=	-Wl",/DYNAMICBASE" -Wl",/LARGEADDRESSAWARE" -Wl",/NODEFAULTLIB" \
-			-Wl",/ENTRY:wWinMain" -Wl",/SUBSYSTEM:WINDOWS"
+			-Wl",/INTEGRITYCHECK" -Wl",/HIGHENTROPYVA" -Wl",/MACHINE:x64"
 
-OPT_Uni	=	
+OPT_Uni	=	-v -H
 
 OPT		=	$(OPT_F) $(OPT_Fno) $(OPT_M) $(OPT_Mno) \
 			$(OPT_W) $(OPT_Wno) $(OPT_Wl) $(OPT_Uni)
 
-CC_Com	=	$(CC) $(OPT)
-CX_Com	=	$(CXX) $(OPT)
+# Compiler for library
+C_LIB	=	$(CC) $(OPT) -c
+CXX_LIB	=	$(CXX) $(OPT) -c
 
+# Compiler for PE (Portable Executable)
+C_PE	=	$(C_LIB)
+CXX_PE	=	$(CXX_LIB)
+
+# Windows LLVM linker configuration
+LINK	=	lld-link \
+			/entry:wWinMain /subsystem:WINDOWS /integritycheck \
+			/largeaddressaware /highentropyva /dynamicbase \
+			/incremental:no /appcontainer:no /nodefaultlib \
+			/machine:x64
+
+# LLVM Archive configuration
+LL_ARC	=	llvm-ar rsUv
+
+# LLVM Strip configuration
+LL_STP	=	llvm-strip -S
