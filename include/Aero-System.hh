e64d28871194924d9c51e22381f82f4dd127502c
rhysqi <rhysqi.akbar@gmail.com>
2025-05-18 16:24:47 +0700

    Add: Types definition - Add custom data types
    submit: rhysqi

--- include/Aero-System.hh
+++ include/Aero-System.hh
@@ -1,15 +1,29 @@
 #ifndef AERO_SYSTEM_HH
 #define AERO_SYSTEM_HH
 
-#include "basetsd.h"
 #ifndef UNICODE
 #define UNICODE
 #endif /* UNICODE */
 
 #if defined(_WIN32) || defined(_WIN64)
 
+#define AERO_USE_MIN_POOL 4
+#define AERO_USE_MAX_POOL (AERO_USE_MIN_POOL << 4)
+
+#define AERO_1_BYTES_OFFSET 1
+#define AERO_2_BYTES_OFFSET 2
+#define AERO_4_BYTES_OFFSET 4
+#define AERO_8_BYTES_OFFSET 8
+
+#define AERO_SSE_BYTES_OFFSET 16
+#define AERO_AVX_BYTES_OFFSET 32
+#define AERO_AVX512_BYTES_OFFSET 64
+
 #include <windows.h>
 #include <winnt.h>
+#include <minwindef.h>
+
+#include <basetsd.h>
 
 namespace Aero_System {
 	// Diagnostic definition
@@ -44,37 +58,76 @@ namespace Aero_System {
 		INT ShowMessage(LPCWSTR lpMessage);
     }
 
-	// Memory Pool definition
+	// Pool definition
 	namespace Pool {
+
+		// Memory definition
 		namespace Memory {
+
+			// Heap definition
 			namespace Heap {
-				LPVOID Create(INT PoolCount, LPVOID lpMemoryInit);
+				typedef struct {
+					LPVOID lpMemoryPool;
+					INT PoolCount;
+					UINT uOffset;
+				} Heap_Pool_t, *pHeap_Pool_t;
+
+				LPVOID Create(INT PoolCount, pHeap_Pool_t pHeapPool);
 				UINT CountPool(LPVOID lpMemoryPool);
-				LPVOID Peek(LPVOID lpMemoryPool, UINT offset);
+				LPVOID Peek(LPVOID lpMemoryPool, UINT uIndex);
 
 				BOOL Lock(LPVOID lpMemoryPool);
+				BOOL LockEx(LPVOID lpMemoryPool, UINT uIndex);
 				BOOL Unlock(LPVOID lpMemoryPool);
+				BOOL UnlockEx(LPVOID lpMemoryPool, UINT uIndex);
 				BOOL Free(LPVOID lpMemoryPool);
 				BOOL Destroy(LPVOID lpMemoryPool);
 			}
 
+			// Virtual definition
 			namespace Virtual {
-				LPVOID Create(INT PoolCount, LPVOID lpMemoryInit);
+				typedef struct {
+					LPVOID lpAddress;
+					SIZE_T dwSize;
+					DWORD dwProtection;
+					DWORD flAllocationType;
+					UINT uOffset;
+				} Virtual_Pool_t, *pVirtual_Pool_t;
+
+				LPVOID Create(INT PoolCount, pVirtual_Pool_t pVirtualPool);
 				UINT CountPool(LPVOID lpMemoryPool);
-				LPVOID Peek(LPVOID lpMemoryPool, UINT offset);
+				LPVOID Peek(LPVOID lpMemoryPool, UINT uIndex);
 
 				BOOL Protect(LPVOID lpMemoryPool, DWORD dwProtection);
+				BOOL ProtectEx(LPVOID lpMemoryPool, DWORD dwProtection, UINT uIndex);
 				BOOL Lock(LPVOID lpMemoryPool);
+				BOOL LockEx(LPVOID lpMemoryPool, UINT uIndex);
 				BOOL Unlock(LPVOID lpMemoryPool);
-				BOOL Free(LPVOID lpMemoryPool);
-				BOOL Destroy(LPVOID lpMemoryPool);
+				BOOL UnlockEx(LPVOID lpMemoryPool, UINT uIndex);
+				BOOL Free(LPVOID lpMemoryPool, DWORD dwFreeType);
 			}
 		}
+	}
 
-		namespace Process {
-			LPBOOL Create(UINT16 uProcessCount, BOOL bProcessInit);
-			LPBOOL Terminate(HANDLE hProcess, DWORD dwExitCode);
-		}
+	// Types definition
+	namespace Types {
+		typedef LPINT *LPPINT;
+		typedef LPBOOL *LPPBOOL;
+		typedef LPCSTR *LPPCSTR;
+		typedef LPCWSTR *LPPCWSTR;
+		typedef LPVOID *LPPVOID;
+		typedef LPWORD *LPPWORD;
+		typedef LPBYTE *LPPBYTE;
+		
+		typedef PINT *PPINT;
+		typedef PFLOAT *PPFLOAT;
+		typedef PBOOL *PPBOOL;
+		typedef PCHAR *PPCHAR;
+		typedef PSTR *PPSTR;
+		typedef PWCHAR *PPWCHAR;
+		typedef PVOID *PPVOID;
+		typedef PWORD *PPWORD;
+		typedef PBYTE *PPBYTE;
 	}
 
 	// Utils definition
